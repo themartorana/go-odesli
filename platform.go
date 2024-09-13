a64a707ba96bcc66404b30a116cbb457c5758e9d
Dave Martorana <dave@flyclops.com>
2024-09-12 22:39:14 -0400

    feat: Platform parsing, strings

--- platform.go
+++ platform.go
@@ -25,3 +25,131 @@ const (
 	PlatformYoutube      Platform = "youtube"
 	PlatformYoutubeMusic Platform = "youtubeMusic"
 )
+
+func (a Platform) String() string {
+	return string(a)
+}
+
+func AvailablePlatforms() []Platform {
+	return []Platform{
+		PlatformAmazon,
+		PlatformAmazonMusic,
+		PlatformAmazonStore,
+		PlatformAnghami,
+		PlatformAppleMusic,
+		PlatformAudiomack,
+		PlatformAudius,
+		PlatformBandcamp,
+		PlatformBoomplay,
+		PlatformDeezer,
+		PlatformGoogle,
+		PlatformItunes,
+		PlatformNapster,
+		PlatformPandora,
+		PlatformSoundcloud,
+		PlatformSpinrilla,
+		PlatformSpotify,
+		PlatformTidal,
+		PlatformYandex,
+		PlatformYoutube,
+		PlatformYoutubeMusic,
+	}
+}
+
+func PlatformFromString(s string) Platform {
+	switch s {
+	case "amazon":
+		return PlatformAmazon
+	case "amazonMusic":
+		return PlatformAmazonMusic
+	case "amazonStore":
+		return PlatformAmazonStore
+	case "anghami":
+		return PlatformAnghami
+	case "appleMusic":
+		return PlatformAppleMusic
+	case "audiomack":
+		return PlatformAudiomack
+	case "audius":
+		return PlatformAudius
+	case "bandcamp":
+		return PlatformBandcamp
+	case "boomplay":
+		return PlatformBoomplay
+	case "deezer":
+		return PlatformDeezer
+	case "google":
+		return PlatformGoogle
+	case "itunes":
+		return PlatformItunes
+	case "napster":
+		return PlatformNapster
+	case "pandora":
+		return PlatformPandora
+	case "soundcloud":
+		return PlatformSoundcloud
+	case "spinrilla":
+		return PlatformSpinrilla
+	case "spotify":
+		return PlatformSpotify
+	case "tidal":
+		return PlatformTidal
+	case "yandex":
+		return PlatformYandex
+	case "youtube":
+		return PlatformYoutube
+	case "youtubeMusic":
+		return PlatformYoutubeMusic
+	default:
+		return Platform("")
+	}
+}
+
+func (a Platform) ReadableName() string {
+	switch a {
+	case PlatformAmazon:
+		return "Amazon"
+	case PlatformAmazonMusic:
+		return "Amazon Music"
+	case PlatformAmazonStore:
+		return "Amazon Store"
+	case PlatformAnghami:
+		return "Anghami"
+	case PlatformAppleMusic:
+		return "Apple Music"
+	case PlatformAudiomack:
+		return "Audiomack"
+	case PlatformAudius:
+		return "Audius"
+	case PlatformBandcamp:
+		return "Bandcamp"
+	case PlatformBoomplay:
+		return "Boomplay"
+	case PlatformDeezer:
+		return "Deezer"
+	case PlatformGoogle:
+		return "Google"
+	case PlatformItunes:
+		return "iTunes"
+	case PlatformNapster:
+		return "Napster"
+	case PlatformPandora:
+		return "Pandora"
+	case PlatformSoundcloud:
+		return "SoundCloud"
+	case PlatformSpinrilla:
+		return "Spinrilla"
+	case PlatformSpotify:
+		return "Spotify"
+	case PlatformTidal:
+		return "Tidal"
+	case PlatformYandex:
+		return "Yandex"
+	case PlatformYoutube:
+		return "YouTube"
+	case PlatformYoutubeMusic:
+		return "YouTube Music"
+	default:
+		return "Unknown"
+	}
+}
